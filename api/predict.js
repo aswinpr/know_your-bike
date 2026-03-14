c78e82f74629d0ae972a39ba2824e2877a601395
aswinpr <aswinpr46@gmail.com>
2026-03-14 10:18:33 +0530

    fix gemini model version

--- api/predict.js
+++ api/predict.js
@@ -42,7 +42,7 @@ funny warning
 `;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-002:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
